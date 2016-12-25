5f4d573f20406f886519a5682998ef21a8121cee
VladCincean <cvie1883@scs.ubbcluj.ro>
2016-12-25 19:27:31 +0200

    d2

---
+++ Day02/ex04/ft_print_comb.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcincean <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/01 09:20:07 by vcincean          #+#    #+#             */
+/*   Updated: 2016/09/01 16:03:35 by vcincean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_put_comb(char i, char j, char k)
+{
+	ft_putchar(i);
+	ft_putchar(j);
+	ft_putchar(k);
+	if (i != '7' || j != '8' || k != '9')
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	i;
+	char	j;
+	char	k;
+
+	i = '0';
+	while (i <= '7')
+	{
+		j = i + 1;
+		while (j <= '8')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				ft_put_comb(i, j, k);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
---
+++ Day02/ex05/ft_print_comb2.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcincean <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/01 10:30:21 by vcincean          #+#    #+#             */
+/*   Updated: 2016/09/01 16:05:04 by vcincean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_put_two_digits(int n)
+{
+	char	a1;
+	char	a2;
+
+	a1 = (char)(n / 10) + '0';
+	a2 = (char)(n % 10) + '0';
+	ft_putchar(a1);
+	ft_putchar(a2);
+}
+
+void	ft_print_comb2(void)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_put_two_digits(i);
+			ft_putchar(' ');
+			ft_put_two_digits(j);
+			if (i != 98 || j != 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j = j + 1;
+		}
+		i = i + 1;
+	}
+}
